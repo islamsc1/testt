e6374a141ba341cd71ab4f8d4696e9b4a5c1182f
islamsc1 <43895797+islamsc1@users.noreply.github.com>
2024-02-15 16:14:18 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
-import {disablereactDevTools} from "@fvilers/disable-react-devtools"
-disablereactDevTools();
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <button class="monthbtn" id="m10Button">October 10/2023</button>
