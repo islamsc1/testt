a71a614bca024119839761a7eca30c22c7f0b6b2
Fatima Ibrahim Ali El Sawy El Gizawy <141643412+fatmaa31022896@users.noreply.github.com>
2024-02-17 00:51:06 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 // import './index.css'
 
 
